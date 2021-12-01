178ded143ecac7afcc9115829295f22bc036ff50
Aidan Urschel <aidanjinkyoo@mail.com>
2021-11-30 19:24:50 -0500

    Pushing Final Changes

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Jin-Kyoos-E-Commerce
+#Jin-Kyoos-E-Commerce
+  
+  ![License](https://img.shields.io/badge/license-MIT-blue)
+
+  
+  ## **Description**
+  
+  This is a backend application for an e-commerce website.
+  
+  ---
+  
+  ## **Table of Contents**
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ---
+  
+  ## **Installation**
+  
+  To install necessary dependencies, run the following command: 
+  
+  	npm install
+  
+  ---
+  
+  ## **Usage**
+  
+  	npm run start
+  
+  ---
+  
+  ## **License**
+  
+  MIT
+  ---
+
+
+  ## **Questions**
+  
+  If you have any questions about the rep, open an issue or contact me directly at [aidanjinkyoo@gmail.com](mailto:aidanjinkyoo@gmail.com). You can find more of my work at [GitHub](https://github.com/A-J-U).
+  
---
+++ models/Product.js
@@ -0,0 +1,53 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require("sequelize");
+// import our database connection from config.js
+const { Category } = require(".");
+
+const sequelize = require("../config/connection");
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Category,
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "product",
+  }
+);
+
+module.exports = Product;
